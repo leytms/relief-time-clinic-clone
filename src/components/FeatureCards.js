aa619feb1e97e8bd1dab919e074b5c86a747e5ee
leytms <mousioselef@gmail.com>
2026-04-26 20:45:16 +0300

    icon-title flex

--- src/components/FeatureCards.js
+++ src/components/FeatureCards.js
@@ -1,9 +1,13 @@
 export default function FeatureCards({ title, description, icon }) {
     return (
         <div className="p-6 border rounded">
+            <div className="flex gap-3 items-center">
+              <h5 className="mt-2 text-2xl">{icon}</h5> 
+              <h4 className="font-semibold">{title}</h4> 
+            </div>
             <h4 className="font-semibold">{title}</h4>
-            <p className="mt-2 text-sm">{description}</p>
-            <h5 className="mt-2 text-2xl">{icon}</h5>
+            
+            
             
         </div>
     )
