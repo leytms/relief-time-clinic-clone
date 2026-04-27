a5b20c4968619783795d14c6a9655677a3cbbbef
leytms <mousioselef@gmail.com>
2026-04-27 18:20:51 +0300

    hover

--- src/components/FeatureCards.js
+++ src/components/FeatureCards.js
@@ -1,12 +1,12 @@
 export default function FeatureCards({ title, description, icon, button }) {
     return (
-        <div className="p-6 border rounded">
+        <div className="p-6 border rounded transition hover:shadow-lg hover:-translate-y-1">
             <div className="flex gap-3 items-center">
               <span className="text-2xl">{icon}</span> 
               <h4 className="font-semibold">{title}</h4> 
             </div>
             <p className="mt-2 text-sm">{description}</p>
-            <button className="mt-4 text=sm font=medium">{button}</button>
+            <button className="mt-4 text-sm font-medium">{button}</button>
         </div>
     )
 
