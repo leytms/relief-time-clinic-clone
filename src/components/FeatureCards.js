537159249593c0731945222ba7901f7faeed9207
leytms <mousioselef@gmail.com>
2026-04-28 19:28:29 +0300

    dynamic styling

--- src/components/FeatureCards.js
+++ src/components/FeatureCards.js
@@ -1,6 +1,12 @@
 export default function FeatureCards({ title, description, icon, button, featured }) {
     return (
-        <div className="p-6 border rounded transition hover:shadow-lg hover:-translate-y-1">
+        <div 
+          className={`
+            p-6 border rounded transition hover:shadow-lg hover:-translate-y-1
+            ${featured ? "border-blue-500" : "border-gray200"}
+            `}
+        >    
+            
             <div className="flex gap-3 items-center">
               <span className="text-2xl">{icon}</span> 
               <h4 className="font-semibold">{title}</h4> 
