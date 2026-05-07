89582ac9f1ee4c2093629270118df703b501151a
leytms <mousioselef@gmail.com>
2026-05-07 23:12:58 +0300

    1st useState

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,10 @@
+"use client"
+
+import { useState } from "react"
 import Container from "@/components/Container"
 
 export default function Hero() {
+  const [isStarted, setIsStarted] = useState(false)
   return (
     <section className="py-16">
       <Container>
@@ -13,9 +17,17 @@ export default function Hero() {
             Fast, simple & secure
           </p>
 
-          <button className="px-4 py-4 mt-6 rounded">
+          <button 
+          onClick={() => setIsStarted(true)}
+          className="px-4 py-4 mt-6 rounded">
             Get Started
           </button>
+
+          {isStarted && (
+            <p className="mt-10 text-sm">
+              Great! You can now continue with your online consultation
+            </p>
+          )}
 
       </Container>
     </section>
