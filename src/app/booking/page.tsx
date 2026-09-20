beea00382d4c28688f02132b4d591b5b8125fa38
leytms <mousioselef@gmail.com>
2026-09-20 21:11:32 +0300

    types

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -28,6 +28,16 @@ export default function BookingPage() {
         <p className="mt-4 text-gray-600">
           Choose a doctor and an available time slot.
         </p>
+        {specialties.map((specialty) =>{
+          return (
+            <button
+            key={specialty.id}
+            className="px-4 py-2 text-gray-600"
+            >
+              {specialty.title}
+            </button>
+          )
+        })}
       </Container>
     </main>
   )
